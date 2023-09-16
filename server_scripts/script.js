422d8d80fc59f903ef89f8ef1c481b6f3b25a35a
Treeway <56928485+Treeway7@users.noreply.github.com>
2023-09-16 17:44:12 -0500

    Update script.js

--- server_scripts/script.js
+++ server_scripts/script.js
@@ -1,6 +1,10 @@
 ServerEvents.recipes((event) => {
+    // Loop through every shapeless water bucket recipe
     event.forEachRecipe({ input: 'minecraft:water_bucket', type: 'minecraft:crafting_shapeless' }, (recipe) => {
-        // Loop through every shapeless water bucket recipe and get its other ingredients.
+        // Blacklist mixing recipes that are already in Create
+        if (recipe.getOriginalRecipeResult() === 'create:dough' || recipe.getOriginalRecipeResult() === 'create:wheat_dough') return;
+
+        // Get existing recipe ingredients, other than the water bucket
         const new_ingredients = [];
         recipe.getOriginalRecipeIngredients().forEach((ingredient) => {
             ingredient.stacks.forEach((recipe_item) => {
@@ -12,14 +16,11 @@ ServerEvents.recipes((event) => {
         // Replace the water bucket with water fluid!
         new_ingredients.push(Fluid.of('minecraft:water', 1000));
 
-        // Blacklist mixing recipes that are already in Create
-        if (recipe.result === 'create:dough' || recipe.result === 'create:wheat_dough') return;
-
         // Add the old recipe's ingredients to a new mixing recipe
         recipe.id(`${recipe.getId()}_manual_only`);
         event.recipes.createMixing(
-            recipe.result,
+            recipe.getOriginalRecipeResult(),
             new_ingredients,
         );
     });
-})
+}
